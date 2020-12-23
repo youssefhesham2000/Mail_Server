7558c526e84ff954e46731c08d5a97d84b707e51
Ahmed Hazem <amhmamgs.2000@gmail.com>
2020-12-23 22:49:26 +0200

    edit

--- BACK/src/main/java/mail/back/Contact.java
+++ BACK/src/main/java/mail/back/Contact.java
@@ -3,4 +3,10 @@ package mail.back;
 public class Contact {
     String first_name, last_name;
     DoubleLinkedList emails;
+
+    public Contact(String first_name, String last_name, DoubleLinkedList emails) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.emails = emails;
+    }
 }
