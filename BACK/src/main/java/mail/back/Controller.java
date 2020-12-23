a52544245efcbb9bc02898490f7759b024ded1ae
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2020-12-23 15:58:55 +0200

    primary controllers

---
+++ BACK/src/main/java/mail/back/Controller.java
@@ -0,0 +1,153 @@
+package eg.edu.alexu.csd.mailServer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.Resource;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import io.netty.handler.codec.http.HttpHeaders;
+
+@RestController
+@CrossOrigin(origins="*",allowedHeaders="*")
+public class Controller {
+	
+	
+//	@PostMapping ("signUP")
+//	public int SignUP(@RequestBody User u ) {
+//		
+//		// return id 
+//		return 0;
+//	}
+
+	@GetMapping("signin/{email}/{pass}")
+	public String getIDAndSignIN(@PathVariable String email,@PathVariable String pass) {
+		
+		/// return user Id + random session number
+		/// return error if user not registered
+	 return null;
+	}	
+	
+	
+	@GetMapping("getEmails/{userID}/{folderName}/{pageNo}")
+	public List<emails> getEmails(@PathVariable int userID ,@PathVariable String folderName ,@PathVariable int pageNo) {
+		
+		
+	  return null;
+	}	
+	
+	
+	@GetMapping("getFolders/{userID}")
+	public List<String> getFolders(@PathVariable int userID) {
+		
+		
+	  return null;
+	}	
+	
+	
+	@GetMapping("getContacts/{userID}")
+	//public List<Contact> getContacts(@PathVariable int userID) {
+		
+		
+	  //return null;
+	}
+	
+	@PutMapping("renameFolder/{userID}/{oldName}/{newName}")
+	public void renameFolder(@PathVariable int userID, @PathVariable String oldName,@PathVariable String newName) {
+			
+		
+	}
+	
+	
+	@PutMapping("addFolder/{userID}/{name}")
+	public void addFolder(@PathVariable int userID, @PathVariable String name) {
+		
+		
+		
+	}
+	
+	
+	@DeleteMapping("deleteEmails/{userID}/{folderName}")
+	public void addFolder(@PathVariable int userID, @PathVariable String folderName,@RequestBody List<Integer> EmailIDs ) {
+		
+			
+	}
+	
+	
+	@DeleteMapping("removeFolder/{userID}/{name}")
+	public void removeFolder(@PathVariable int userID, @PathVariable String name) {
+		
+		
+		
+	}
+	
+	
+	
+	//////////
+	//  delete file in path: "attachments\\" +name
+	///
+	@DeleteMapping("deleteAttachment/{name}")
+	public void deleteAttachment(@PathVariable String name) {
+		
+	}
+	
+	@GetMapping("/download/{fileName:.+}")
+	public ResponseEntity downloadFileFromLocal(@PathVariable String fileName) {
+		File file=new File("attachments\\"+fileName) ; 
+		InputStreamResource resource = null;
+		try {
+			resource = new InputStreamResource(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		    return ResponseEntity.ok()
+		            .contentLength(file.length())
+		            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+		            .body(resource);
+		    
+	}
+	
+	  @PostMapping("/upload/{userID}/{emailID}" )
+	  public void fileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+	 
+		  		try {
+				// read and write the file to the selected location-
+				byte[] bytes = file.getBytes();
+				Path path = Paths.get("attachments\\" + file.getOriginalFilename());
+				/// making a file like the one that was uploaded 
+				Files.write(path, bytes);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+		}
+
+		}
+	
+	
+	
+	
+}
