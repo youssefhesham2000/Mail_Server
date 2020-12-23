8b6a28f955916e8c8947168b864c80a220a15d2b
youssefhesham2000 <58773494+youssefhesham2000@users.noreply.github.com>
2020-12-23 07:49:40 +0200

    main page added to router

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import email from './components/email'
 import compose from './components/compose'
-
+import mainPage from './components/mainPage'
 import table from './components/table'
 import HelloWorld from './components/HelloWorld'
 import Contacts from './components/Contacts'
@@ -46,6 +46,11 @@ export default new Router({
       path: '/EmailModification',
       name: 'EmailModification',
       component: EmailModification
+    },
+    {
+      path: '/mainPage',
+      name: 'mainPage',
+      component: mainPage
     },
        {
       path: '/folders/:name',
